0034797285f3cce4bce84ad8bdb2ac915bb53f81
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-26 21:44:46 +0300

    make build

---
+++ index.5bbc897b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider");e.style.transform="translate(-50%, -50%)",e.style.top="50%",e.style.left="50%";
+//# sourceMappingURL=index.5bbc897b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+const spider = document.querySelector(".spider");
+spider.style.transform = "translate(-50%, -50%)";
+spider.style.top = "50%";
+spider.style.left = "50%";
+
+//# sourceMappingURL=index.f75de5e1.js.map
